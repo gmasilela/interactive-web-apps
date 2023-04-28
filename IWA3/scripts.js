6c2a499f56c8a3eba334715e4363a55cd7c03668
gmasilela <g.masilela@lab.clicklearning.org>
2023-04-28 20:35:21 +0200

    challenge1

---
+++ IWA3/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+import {company} from 'configuration'
+import {year} from 'configuration'
+
+
+const message = '© ' + company + ' (' + year + ')'
+document.querySelector('footer').innerText = message
