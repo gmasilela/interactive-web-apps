fdac1b86b1eff545844a4ccd985426bae4649ae8
gmasilela <g.masilela@lab.clicklearning.org>
2023-05-10 12:47:05 +0200

    fixed the code on challenge2

--- IWA6/Challenge2/scripts.js
+++ IWA6/Challenge2/scripts.js
@@ -3,15 +3,16 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 0;
-const minuteOfDay = 0;
+const hourOfDay = 00;
+const minuteOfDay = 00;
 
 // Only change below this line
 
-if (hourOfDay === 0 && minuteOfDay === 0) {
-  const taxAsDecimal = parseFloat(tax) / 100;
-  const startingAfterTax = salary * (1 - taxAsDecimal);
-  const balance = startingAfterTax - transport - food - rent;
-  
-  console.log('R', balance.toFixed(2));
+if (hourOfDay == 00 && minuteOfDay == 00) {
+    const taxAsDecimal = parseFloat(tax) / 100;
+    const startingAfterTax = salary * (1 - taxAsDecimal);
+	const balance = startingAfterTax - transport - food - rent;
+
+	
+console.log(balance.toFixed(3))
 }
