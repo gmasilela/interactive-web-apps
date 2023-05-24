fa30399321af830315bec91d21dfea38255049a7
gmasilela <g.masilela@lab.clicklearning.org>
2023-05-24 10:14:24 +0200

    Fixed The Code

--- IWA7/Challenge3/hjgj.js
+++ IWA7/Challenge3/hjgj.js
@@ -1,20 +1,19 @@
-const leoName = 'Leo'
-const leoSurname = 'Musvaire     '
-const leoBalance = '-9394'
+const leoName = 'Leo';
+const leoSurname = 'Musvaire     ';
+const leoBalance = '-9394';
 
-const sarahName = 'Sarah    '
-const sarahSurname = 'Kleinhans'
-const sarahBalance = '-4582.21000111'
+const sarahName = 'Sarah    ';
+const sarahSurname = 'Kleinhans';
+const sarahBalance = '-4582.21000111';
 
-const divider = '----------------------------------'
+const divider = '----------------------------------';
 
 // Only change below this line
 const leoOwed = parseFloat(leoBalance).toFixed(2);
 const sarahOwed = parseFloat(sarahBalance).toFixed(2);
 const totalOwed = Math.abs(parseFloat(leoBalance) + parseFloat(sarahBalance)).toFixed(2);
 
-const result =
-`
+const result = `
 ${leoName} ${leoSurname.trim()} (Owed: R ${leoOwed})
 ${sarahName} ${sarahSurname.trim()} (Owed: R ${sarahOwed})
 
@@ -23,4 +22,4 @@ ${divider}
 ${divider}
 `;
 
-console.log(result)
+console.log(totalOwed);
