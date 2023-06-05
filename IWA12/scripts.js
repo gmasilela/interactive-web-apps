92443dbcf226d0184f02a3e0177205abf6daac53
gmasilela <g.masilela@lab.clicklearning.org>
2023-06-05 13:40:31 +0200

    fixed code IWA12

--- IWA12/scripts.js
+++ IWA12/scripts.js
@@ -29,35 +29,42 @@ const STATUS_MAP = {
 
 // Edit below line 
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+const status1 = document.querySelector('#book1 .status');
+const reserve1 = document.querySelector('#book1 .reserve');
+const checkout1 = document.querySelector('#book1 .checkout');
+const checkin1 = document.querySelector('#book1 .checkin');
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+const status2 = document.querySelector('#book2 .status');
+const reserve2 = document.querySelector('#book2 .reserve');
+const checkout2 = document.querySelector('#book2 .checkout');
+const checkin2 = document.querySelector('#book2 .checkin');
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+const status3 = document.querySelector('#book3 .status');
+const reserve3 = document.querySelector('#book3 .reserve');
+const checkout3 = document.querySelector('#book3 .checkout');
+const checkin3 = document.querySelector('#book3 .checkin');
 
-checkin.0.color = none
-status.0.style.color = STATUS_MAP.status.color
-reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+// Update the status, button properties, and styles
+status1.style.color = STATUS_MAP.overdue.color;
+reserve1.disabled = !STATUS_MAP.overdue.canReserve;
+checkout1.disabled = !STATUS_MAP.overdue.canCheckout;
+checkin1.disabled = !STATUS_MAP.overdue.canCheckIn;
+reserve1.style.filter = 'grayscale(100%)';
+checkout1.style.filter = 'grayscale(100%)';
+checkin1.style.filter = 'grayscale(100%)';
 
-checkin.1.color = none
-status.1.style.color = STATUS_MAP.status.color
-reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+status2.style.color = STATUS_MAP.reserved.color;
+reserve2.disabled = !STATUS_MAP.reserved.canReserve;
+checkout2.disabled = !STATUS_MAP.reserved.canCheckout;
+checkin2.disabled = !STATUS_MAP.reserved.canCheckIn;
+reserve2.style.filter = 'grayscale(100%)';
+checkout2.style.filter = 'grayscale(100%)';
+checkin2.style.filter = 'grayscale(100%)';
 
-checkin.2.color = none
-status.2.style.color = STATUS_MAP.status.color
-reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+status3.style.color = STATUS_MAP.shelf.color;
+reserve3.disabled = !STATUS_MAP.shelf.canReserve;
+checkout3.disabled = !STATUS_MAP.shelf.canCheckout;
+checkin3.disabled = !STATUS_MAP.shelf.canCheckIn;
+reserve3.style.filter = 'grayscale(100%)';
+checkout3.style.filter = 'grayscale(100%)';
+checkin3.style.filter = 'grayscale(100%)';
