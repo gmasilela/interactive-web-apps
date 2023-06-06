34188a0380260c1dc03da1a3f04461887899cd9f
gmasilela <g.masilela@lab.clicklearning.org>
2023-06-06 14:42:46 +0200

    Fixed code IWA11

--- IWA11/script.js
+++ IWA11/script.js
@@ -1,27 +1,22 @@
 // script.js
 
 const order1 = document.querySelector('[data-key="order1"]');
-const order2 = document.querySelector('[data-key="order2"]');
-const order3 = document.querySelector('[data-key="order3"]');
-
-
 const biscuits1 = order1.querySelector('.biscuits .count');
 const donuts1 = order1.querySelector('.donuts .count');
 const pancakes1 = order1.querySelector('.pancakes .count');
 const status1 = order1.querySelector('.status dd');
 
-
+const order2 = document.querySelector('[data-key="order2"]');
 const biscuits2 = order2.querySelector('.biscuits .count');
 const donuts2 = order2.querySelector('.donuts .count');
 const pancakes2 = order2.querySelector('.pancakes .count');
 const status2 = order2.querySelector('.status dd');
 
-
+const order3 = document.querySelector('[data-key="order3"]');
 const biscuits3 = order3.querySelector('.biscuits .count');
 const donuts3 = order3.querySelector('.donuts .count');
 const pancakes3 = order3.querySelector('.pancakes .count');
 const status3 = order3.querySelector('.status dd');
-
 
 biscuits1.textContent = order1.dataset.biscuits;
 donuts1.textContent = order1.dataset.donuts;
@@ -39,4 +34,3 @@ biscuits3.textContent = order3.dataset.biscuits;
 donuts3.textContent = order3.dataset.donuts;
 pancakes3.textContent = order3.dataset.pancakes;
 status3.textContent = order3.dataset.delivered === 'true' ? 'Delivered' : 'Pending';
-
